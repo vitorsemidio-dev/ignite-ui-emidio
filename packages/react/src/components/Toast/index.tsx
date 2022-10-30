f5029a342bb90f898f139a0744baf63ea09d91e8
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-10-30 14:16:25 -0300

    ação clique

--- packages/react/src/components/Toast/index.tsx
+++ packages/react/src/components/Toast/index.tsx
@@ -1,3 +1,25 @@
+import * as ToastPrimitive from '@radix-ui/react-toast'
+import { useState } from 'react'
+
 export function Toast() {
-  return <div>Eu sou um Toast</div>
+  const [open, setOpen] = useState(false)
+  return (
+    <ToastPrimitive.Provider>
+      <button
+        onClick={() => {
+          console.log('clicou')
+          setOpen(true)
+        }}
+      >
+        Add to calendar
+      </button>
+      <ToastPrimitive.Root type="foreground" open={open} onOpenChange={setOpen}>
+        <ToastPrimitive.Title>Título</ToastPrimitive.Title>
+        {/* <ToastPrimitive.Description>Saved!</ToastPrimitive.Description>
+        <ToastPrimitive.Close aria-label="Close">
+          <span aria-hidden>×</span>
+        </ToastPrimitive.Close> */}
+      </ToastPrimitive.Root>
+    </ToastPrimitive.Provider>
+  )
 }
