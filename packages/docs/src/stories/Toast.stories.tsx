7b3159f1c5c8ddea1d1bf363690d15dc721577ec
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-10-30 14:00:29 -0300

    criação story toast

---
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Toast } from '@ignite-ui-emidio/react'
+
+export default {
+  title: 'Overlay/Toast',
+  component: Toast
+} as Meta<any>
+
+export const Primary: StoryObj<any> = {}
