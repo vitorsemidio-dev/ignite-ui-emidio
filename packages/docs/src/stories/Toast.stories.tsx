75e34c73497844bf815464ff894e4946b819fa4b
Vitor Emídio <vitorsemidio.96@gmail.com>
2022-10-30 14:28:11 -0300

    copia demo

--- packages/docs/src/stories/Toast.stories.tsx
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Toast } from '@ignite-ui-emidio/react'
+import { ToastDemo } from '@ignite-ui-emidio/react'
 
 export default {
   title: 'Overlay/Toast',
-  component: Toast
+  component: ToastDemo
 } as Meta<any>
 
 export const Primary: StoryObj<any> = {}
