9bb9f03175349b0a5556ea3cfd279c15c6fdbf67
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-04-25 01:02:05 -0300

    :pencil2: deploy documentação storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,8 +1,11 @@
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react'
-import { defineConfig} from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins:[react()]
-})
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
