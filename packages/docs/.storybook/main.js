2c34df1f852976af665419b02a188ae29b74771f
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-06-11 01:41:18 -0300

    fix: host storybook docs (#4)
    * chore: downgrade storybook 6
    
    * fix: set framekwork @storybook/react

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
+  "core": {
+    "builder": "@storybook/builder-vite",
   },
+  framework: "@storybook/react",
   "features": {
     "storyStoreV7": true
   },
+  staticDirs: ["../public"],
   viteFinal: (config, {
     configType
   }) => {
@@ -16,7 +17,4 @@ module.exports = {
     }
     return config;
   },
-  docs: {
-    autodocs: true
-  }
 };
