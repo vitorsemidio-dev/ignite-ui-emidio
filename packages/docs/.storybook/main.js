5caacf8a1bf0d50aa2fa709cdf7e47db6ff062be
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-11 00:35:27 -0300

    chore: downgrade storybook 6

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "core": {
+    "builder": "@storybook/builder-vite",
+  },
   "framework": {
     name: "@storybook/react-vite",
     options: {}
@@ -8,6 +11,7 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
+  staticDirs: ["../public"],
   viteFinal: (config, {
     configType
   }) => {
